254be0504faf08dfe868d2705785aeecd4fdf3e0
Muhammad <132049921+qorahoja@users.noreply.github.com>
2023-11-02 17:46:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
-import subprocess  # Import subprocess to run the script
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from @BotFather on Telegram
-BOT_TOKEN = '6141302747:AAFNdXtwbeLjs8Ii8jSvQnMv7Jc1A-RwIKQ'
+BOT_TOKEN = '6141302747:AAFNdXtwbeLjs8Ii8jSvQnMv7Jc1A-RwIKQ'  # Replace with your actual bot token
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -19,8 +18,6 @@ dp.middleware.setup(LoggingMiddleware())
 async def on_start(message: types.Message):
     await message.answer("Assalomu alaykom men USA track musiqa kanalinign boti bo'laman iltimos savol yoki takliflaringiz bo'lsa yozib qoldiring sizga adminimiz javob yozadi")
 
-
-
-if __name__ == '__main':
+if __name__ == '__main__':
     from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
